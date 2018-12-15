292043340fa1943f89203f4344e23af22e8d0896
pbgaming <44928010+pbgaming@users.noreply.github.com>
2018-12-16 00:52:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Dream | ${client.guilds.size} ${client.users.size} servers/user`,"http://twitch.tv/YouTube")
+client.user.setGame(`Made By ╲⎝⧹PBGAMING | Five🌟⧸⎠╱`,"http://twitch.tv/YouTube")
 client.user.setStatus("dnd")
 });
 
